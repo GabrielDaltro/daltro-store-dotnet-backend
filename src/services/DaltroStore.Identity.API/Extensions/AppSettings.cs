82753a3556b95a9872909dbb6f5b07ffe97c7e0e
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-04-30 11:52:03 -0300

    configured jtw generation

---
+++ src/services/DaltroStore.Identity.API/Extensions/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace DaltroStore.Identity.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; } = string.Empty;
+
+        public int ExpirationHours { get; set; }
+
+        public string Issuer { get; set; } = string.Empty;
+
+        public string Audience { get; set; } = string.Empty;
+    }
+}
--- src/services/DaltroStore.Identity.API/Program.cs
+++ src/services/DaltroStore.Identity.API/Program.cs
@@ -1,6 +1,11 @@
 using DaltroStore.Identity.API.Data;
+using DaltroStore.Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,14 +19,45 @@ builder.Services
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+IConfigurationSection appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+AppSettings appSettings = builder.Configuration.Get<AppSettings>() ?? throw new Exception("AppSettings is null");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    bearerOptions.RequireHttpsMetadata = true;
+    bearerOptions.SaveToken = true;
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettings.Secret)),
+        
+        ValidateIssuer = true,
+        ValidIssuer = appSettings.Issuer,
+
+        ValidateAudience = true,
+        ValidAudience = appSettings.Audience
+    };
+});
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "DaltroStore Identity API"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

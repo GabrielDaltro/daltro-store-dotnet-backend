abcbbb6dbc1c2db8905f8c5e47baf83ab4c657f6
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-07-02 11:39:03 -0300

    added Display propety in UserLogin and UserRegister dtos

--- src/services/DaltroStore.Identity.API/Models/UserLoginDto.cs
+++ src/services/DaltroStore.Identity.API/Models/UserLoginDto.cs
@@ -2,12 +2,15 @@
 
 namespace DaltroStore.Identity.API.Models
 {
+    [Display(Name = "UserLogin")]
     public class UserLoginDto
     {
+        [Display(Name = "email")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; } = string.Empty;
 
+        [Display(Name = "password")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(maximumLength: 30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
--- src/services/DaltroStore.Identity.API/Models/UserRegisterDto.cs
+++ src/services/DaltroStore.Identity.API/Models/UserRegisterDto.cs
@@ -1,20 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DaltroStore.Identity.API.Models
 {
+    [DisplayName("UserRegister")]
     public class UserRegisterDto
     {
+        [Display(Name = "name")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get; set; } = string.Empty;
 
+        [Display(Name ="email")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; } = string.Empty;
 
+        [Display(Name = "password")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(maximumLength: 30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
 
+        [Display(Name = "confirmpassword")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [Compare(nameof(Password),ErrorMessage = "As senhas não conferem")]
         public string ConfirmPassword {get; set; } = string.Empty;

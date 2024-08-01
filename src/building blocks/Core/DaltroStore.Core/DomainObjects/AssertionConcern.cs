0cc0ec95fd82ab1a82089f8727aad36f5eeb61ec
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-01 10:01:13 -0300

    created AssertionConcern class

---
+++ src/building blocks/Core/DaltroStore.Core/DomainObjects/AssertionConcern.cs
@@ -0,0 +1,17 @@
+﻿namespace DaltroStore.Core.DomainObjects
+{
+    public static class AssertionConcern
+    {
+        public static void AssertNotEmpty(string str, string message)
+        {
+            if (str.Trim().Length == 0)
+                throw new DomainException(message);
+        }
+
+        public static void AssertNotNull(object obj, string message)
+        {
+            if (obj == null)
+                throw new DomainException(message);
+        }
+    }
+}

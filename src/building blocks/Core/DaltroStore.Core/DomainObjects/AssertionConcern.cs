aeff626125061382c4e8aac25345a82f40bbed39
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-01 12:14:57 -0300

    implemented AssertIsGreaterThan method in AssertionConcern class

--- src/building blocks/Core/DaltroStore.Core/DomainObjects/AssertionConcern.cs
+++ src/building blocks/Core/DaltroStore.Core/DomainObjects/AssertionConcern.cs
@@ -2,15 +2,27 @@
 {
     public static class AssertionConcern
     {
-        public static void AssertNotEmpty(string str, string message)
+        public static void AssertIsNotEmpty(string str, string message)
         {
             if (str.Trim().Length == 0)
                 throw new DomainException(message);
         }
 
-        public static void AssertNotNull(object obj, string message)
+        public static void AssertIsNotNull(object obj, string message)
         {
             if (obj == null)
+                throw new DomainException(message);
+        }
+
+        public static void AssertIsGreaterThan<T>(T actual, T expcted, string message) where T : IComparable
+        {
+            if (actual.CompareTo(expcted) <= 0)
+                throw new DomainException(message);
+        }
+
+        public static void AssertIsGreaterOrEqualsThan<T>(T actual, T expcted, string message) where T : IComparable
+        {
+            if (actual.CompareTo(expcted) < 0)
                 throw new DomainException(message);
         }
     }

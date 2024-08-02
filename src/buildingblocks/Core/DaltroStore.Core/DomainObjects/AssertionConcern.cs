ea02bb47469bad92e200aeaca8db53e4e3114c07
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-02 12:56:39 -0300

    AssertIsEquals and AssertIsNotEquals methods were inplemented in AssertionConcern class

--- src/buildingblocks/Core/DaltroStore.Core/DomainObjects/AssertionConcern.cs
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/AssertionConcern.cs
@@ -25,5 +25,25 @@
             if (actual.CompareTo(expcted) < 0)
                 throw new DomainException(message);
         }
+
+        public static void AssertIsEquals(object actual, object expected, string message)
+        {
+            if (actual == null && expected == null)
+                return;
+            if (actual == null || expected == null)
+                throw new DomainException(message);
+            if (!actual.Equals(expected))
+                throw new DomainException(message);
+        }
+
+        public static void AssertIsNotEquals(object actual, object expected, string message)
+        {
+            if (actual == null && expected == null)
+                throw new DomainException(message);
+            if (actual == null || expected == null)
+                return;
+            if (actual.Equals(expected))
+                throw new DomainException(message);
+        }
     }
 }

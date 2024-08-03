afb3c00133fd38b81f02180683a8d3d550138dcb
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-03 11:26:34 -0300

    implemented ICategoryRepository and IProductRepository

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Business/Repositories/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using DaltroStore.ProductCatalog.Domain.Models;
+
+namespace DaltroStore.ProductCatalog.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+
+        Task<Category> GetById(Guid id);
+        
+        Task<Category> GetByName(string name);
+        
+        Task<Category> GetByCode(int code);
+
+        Task Add(Category category);
+
+        Task Update(Category category);
+
+        Task DeleteById(Guid id);
+    }
+}
---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Business/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using DaltroStore.ProductCatalog.Domain.Models;
+
+namespace DaltroStore.ProductCatalog.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+
+        Task<Product> GetById(Guid id);
+
+        Task<IEnumerable<Product>> GetByCategory(Guid categoryId);
+
+        Task Add(Product category);
+
+        Task Update(Product category);
+
+        Task DeleteById(Guid id);
+    }
+}

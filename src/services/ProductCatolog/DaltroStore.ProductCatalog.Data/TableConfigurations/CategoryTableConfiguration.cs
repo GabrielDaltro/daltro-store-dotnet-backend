f2a11aa3b9976d39a5a0917e8449cbe8058cf07c
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-03 16:08:27 -0300

    implemented CategoryTableConfiguration and ProductTableConfiguration

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/TableConfigurations/CategoryTableConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DaltroStore.ProductCatalog.Domain.Models;
+
+namespace DaltroStore.ProductCatalog.Infrastructure.TableConfigurations
+{
+    internal class CategoryTableConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Code)
+                .IsRequired();
+
+            builder.HasIndex(c => c.Name)
+                .IsUnique();
+
+            builder.HasIndex(c => c.Code)
+                .IsUnique();
+        }
+    }
+}
---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/TableConfigurations/ProductTableConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DaltroStore.ProductCatalog.Domain.Models;
+
+namespace DaltroStore.ProductCatalog.Infrastructure.TableConfigurations
+{
+    internal class ProductTableConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Price)
+                .IsRequired();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(p => p.Active)
+                .IsRequired();
+
+            builder.Property(p => p.Image)
+                .IsRequired()
+                .HasMaxLength(128);
+
+            builder.Property(p => p.CategoryId)
+                .IsRequired();
+
+            builder.Property(p => p.StockQuantity)
+                .IsRequired();
+
+            builder.Property(p => p.RegistrationDate)
+                .IsRequired();
+
+            builder.Property(p => p.Weight)
+                .IsRequired();
+
+            builder.OwnsOne(p => p.Dimension, dimension =>
+            {
+                dimension.Property(d => d.Height)
+                .HasColumnName("Height")
+                .HasColumnType("decimal")
+                .IsRequired();
+
+                dimension.Property(d => d.Width)
+                .HasColumnName("Width")
+                .HasColumnType("decimal")
+                .IsRequired();
+
+                dimension.Property(d => d.Depth)
+                .HasColumnName("Depth")
+                .HasColumnType("decimal")
+                .IsRequired();
+            });
+        }
+    }
+}

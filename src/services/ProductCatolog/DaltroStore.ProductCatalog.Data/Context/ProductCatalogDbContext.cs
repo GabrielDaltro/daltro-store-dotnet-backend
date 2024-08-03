6a0f142ad4a4c679b40df19c20bcfd7d7e8e43c2
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-03 11:49:22 -0300

    installed EF Core 8. Implemented ProductCatalogDbContext

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/Context/ProductCatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DaltroStore.ProductCatalog.Infrastructure.Context
+{
+    internal class ProductCatalogDbContext : DbContext
+    {
+        public ProductCatalogDbContext(DbContextOptions<ProductCatalogDbContext> options) : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductCatalogDbContext).Assembly);
+        }
+    }
+}

3f0ae0a68f760bbfbeb6f1545dce7ada78af6b56
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-03 11:52:20 -0300

    ProductCatalogDbContext implements IUnitOfWork interface

--- src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/Context/ProductCatalogDbContext.cs
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/Context/ProductCatalogDbContext.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DaltroStore.Core.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DaltroStore.ProductCatalog.Infrastructure.Context
 {
-    internal class ProductCatalogDbContext : DbContext
+    internal class ProductCatalogDbContext : DbContext, IUnitOfWork
     {
         public ProductCatalogDbContext(DbContextOptions<ProductCatalogDbContext> options) : base(options){ }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductCatalogDbContext).Assembly);
+        }
+
+        public async Task Commit()
+        {
+           await SaveChangesAsync();
         }
     }
 }

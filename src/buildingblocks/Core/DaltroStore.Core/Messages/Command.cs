0f12b35f991149cbd2eb3aef86afe438ae6d20a0
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-06 12:51:19 -0300

    created command base class

---
+++ src/buildingblocks/Core/DaltroStore.Core/Messages/Command.cs
@@ -0,0 +1,14 @@
+﻿namespace DaltroStore.Core.Messages
+{
+    public abstract class Command
+    {
+        public Guid AggregateId { get; init; }
+
+        public DateTime DateTime { get; init; }
+
+        protected Command()
+        {
+            DateTime = DateTime.UtcNow;
+        }
+    }
+}

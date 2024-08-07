bb309cbf38aef56aeed19760a1e805103c001c86
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-07 13:42:36 -0300

    Created RegisterProductCommand and RegisterProductCommandHandler

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Commands/RegisterProductCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using DaltroStore.Core.Communication;
+
+namespace DaltroStore.ProductCatalog.Application.Commands
+{
+    public class RegisterProductCommandHandler : ICommandHandler<RegisterProductCommand, bool>
+    {
+        public Task<bool> Handle(RegisterProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

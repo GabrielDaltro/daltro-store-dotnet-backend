a7fcfe0bfcfaf85178975733762d4824fee8b541
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-08 16:15:13 -0300

    created CommandResult

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Commands/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿namespace DaltroStore.ProductCatalog.Application.Commands
+{
+    public class CommandResult
+    {
+        public bool Success { get; init; }
+
+        public string Error { get; init; }
+
+        public CommandResult(bool success, string error)
+        {
+            Success = success;
+            Error = error;
+        }
+    }
+}

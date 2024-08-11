1993733ffdad8e1873dbae47eeb47b4797c29b60
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-11 13:38:39 -0300

    created IncreaseProductStockCommand

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Commands/IncreaseProductStockCommand.cs
@@ -0,0 +1,14 @@
+﻿using DaltroStore.Core.Messages;
+
+namespace DaltroStore.ProductCatalog.Application.Commands
+{
+    public class IncreaseProductStockCommand : Command<CommandResult>
+    {
+        public uint Quantity { get; init; }
+
+        public IncreaseProductStockCommand(uint quantity)
+        {
+            Quantity = quantity;
+        }
+    }
+}

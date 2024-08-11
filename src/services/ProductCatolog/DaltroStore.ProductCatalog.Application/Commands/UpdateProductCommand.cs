ef4d8606829c0ba30dd1db23f3f3937e2f9a7a26
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-10 23:01:13 -0300

    created UpdateProductCommand

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Commands/UpdateProductCommand.cs
@@ -0,0 +1,42 @@
+﻿using DaltroStore.Core.Messages;
+
+namespace DaltroStore.ProductCatalog.Application.Commands
+{
+    public class UpdateProductCommand : Command<CommandResult>
+    {
+        public string Name { get; init; }
+
+        public string Description { get; init; }
+
+        public bool Active { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string Image { get; init; }
+
+        public Guid CategoryId { get; init; }
+
+        public decimal Weight { get; init; }
+
+        public decimal Width { get; init; }
+
+        public decimal Height { get; init; }
+
+        public decimal Depth { get; init; }
+
+        public UpdateProductCommand(string name, decimal price, string description, bool active, string image,
+                    Guid categoryId, decimal weight, decimal width, decimal height, decimal depth)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Active = active;
+            Image = image;
+            CategoryId = categoryId;
+            Weight = weight;
+            Width = width;
+            Height = height;
+            Depth = depth;
+        }
+    }
+}

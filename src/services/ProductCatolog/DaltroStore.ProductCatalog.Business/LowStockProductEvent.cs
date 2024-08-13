03e962f1901a8a0aae411b4605776d500a8a14d6
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-13 08:51:11 -0300

    implemented LowStockProductEvent domain event

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Business/LowStockProductEvent.cs
@@ -0,0 +1,14 @@
+﻿using DaltroStore.Core.DomainObjects;
+
+namespace DaltroStore.ProductCatalog.Domain
+{
+    internal class LowStockProductEvent : DomainEvent
+    {
+        public int QuantityRemain { get; init; }
+
+        public LowStockProductEvent(Guid AggregateId, int quantityRemain) : base(AggregateId)
+        {
+            QuantityRemain = quantityRemain;
+        }
+    }
+}

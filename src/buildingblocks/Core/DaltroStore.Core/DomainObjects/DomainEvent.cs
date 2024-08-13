a8683e15bc7e073470928479998b18d93771c2c2
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-13 08:35:11 -0300

    created DomainEvent class

---
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/DomainEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace DaltroStore.Core.DomainObjects
+{
+    public class DomainEvent
+    {
+        public Guid AggregateId { get; init; }
+
+        public DateTime DateTime { get; init; }
+
+        public DomainEvent()
+        {
+            DateTime = DateTime.UtcNow;
+        }
+    }
+}

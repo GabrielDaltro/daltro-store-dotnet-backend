db682cd392f30745c73bb7aab70c28562bc67401
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-14 17:10:53 -0300

    added constructors in EntityNotFoundDomainException and EntityValidationDomainException

--- src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityNotFoundDomainException.cs
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityNotFoundDomainException.cs
@@ -1,4 +1,11 @@
 ﻿namespace DaltroStore.Core.DomainObjects
 {
-    public class EntityNotFoundDomainException : DomainException { }
+    public class EntityNotFoundDomainException : DomainException 
+    {
+        public EntityNotFoundDomainException() { }
+
+        public EntityNotFoundDomainException(string message) : base(message) { }
+
+        public EntityNotFoundDomainException(string message, Exception innerException) : base(message, innerException) { }
+    }
 }
--- src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityValidationDomainException.cs
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityValidationDomainException.cs
@@ -1,4 +1,12 @@
 ﻿namespace DaltroStore.Core.DomainObjects
 {
-    public class EntityValidationDomainException : DomainException { }
+    public class EntityValidationDomainException : DomainException 
+    {
+        public EntityValidationDomainException() { }
+
+        public EntityValidationDomainException(string message) : base(message) { }
+
+        public EntityValidationDomainException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
 }

b7ac2997ceb7743f384d8297e28ec15f50cd846f
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-15 08:44:15 -0300

    The EntityValidationDomainException was renamed to EntityValidationException

---
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace DaltroStore.Core.DomainObjects
+{
+    public class EntityValidationException : DomainException 
+    {
+        public EntityValidationException() { }
+
+        public EntityValidationException(string message) : base(message) { }
+
+        public EntityValidationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}

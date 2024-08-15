b84eb59f90322087210d3562ca70736ff3106566
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-15 08:43:17 -0300

    the EntityNotFoundDomainException class was renamed to EntityNotFoundException

---
+++ src/buildingblocks/Core/DaltroStore.Core/DomainObjects/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace DaltroStore.Core.DomainObjects
+{
+    public class EntityNotFoundException : DomainException 
+    {
+        public EntityNotFoundException() { }
+
+        public EntityNotFoundException(string message) : base(message) { }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}

9e42522ffefd9538ff1496d486c9a0caca10ef81
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-20 11:17:45 -0300

    implemented QueryBus

---
+++ src/buildingblocks/Core/DaltroStore.Core/Communication/Query/QueryBus.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace DaltroStore.Core.Communication.Query
+{
+    public class QueryBus : IQueryBus
+    {
+        private readonly IMediator mediator;
+
+        public QueryBus(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<TViewModel> Send<TQuery, TViewModel>(TQuery query, CancellationToken cancellationToken = default) where TQuery : IQuery<TViewModel>
+        {
+            return await mediator.Send(query, cancellationToken);
+        }
+    }
+}

7c3c09ae39069a01340e60eab5ad0fa0aac114dd
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-21 20:33:00 -0300

    implemented ProductViewModel

--- src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Queries/ViewModels/ProductViewModel.cs
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Queries/ViewModels/ProductViewModel.cs
@@ -1,4 +1,38 @@
 ﻿namespace DaltroStore.ProductCatalog.Application.Queries.ViewModels
 {
-    internal class ProductViewModel { }
+    public class ProductViewModel 
+    {
+        public string Name { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string Description { get; init; }
+
+        public string Category { get; init; }
+
+        public string Image { get; init; }
+
+        public decimal Weight { get; init; }
+
+        public decimal Width { get; init; }
+
+        public decimal Height { get; init; }
+
+        public decimal Depth { get; init; }
+
+        public ProductViewModel(string name, decimal price, string description, 
+                                string image, decimal weight, decimal width,
+                                decimal height, decimal depth, string category)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Image = image;
+            Weight = weight;
+            Width = width;
+            Height = height;
+            Depth = depth;
+            Category = category;
+        }
+    }
 }

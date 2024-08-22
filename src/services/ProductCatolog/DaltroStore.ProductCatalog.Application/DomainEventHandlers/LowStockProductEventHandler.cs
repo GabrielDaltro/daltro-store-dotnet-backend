81677289b1cc73f94382fd828155c838b560b94d
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-22 19:41:02 -0300

    created empty LowStockProductEventHandler

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/DomainEventHandlers/LowStockProductEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using DaltroStore.Core.Communication.DomainEvent;
+using DaltroStore.ProductCatalog.Domain;
+
+namespace DaltroStore.ProductCatalog.Application.DomainEventHandlers
+{
+    internal class LowStockProductEventHandler : IDomainEventHandler<LowStockProductEvent>
+    {
+        public Task Handle(LowStockProductEvent request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

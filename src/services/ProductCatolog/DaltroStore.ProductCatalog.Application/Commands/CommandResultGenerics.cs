62054ba26a17bbb465c07f572120a5c323e3865e
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-23 09:23:10 -0300

    created genericsCommandResult

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Application/Commands/CommandResultGenerics.cs
@@ -0,0 +1,12 @@
+﻿namespace DaltroStore.ProductCatalog.Application.Commands
+{
+    public class CommandResult<T> : CommandResult
+    {
+        public T Value { get; init; }
+
+        public CommandResult(CmdResultStatus status, T value, string error) : base(status, error)
+        {
+            this.Value = value;
+        }
+    }
+}

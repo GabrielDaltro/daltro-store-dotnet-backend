11b4ebea1c8878b67979f5ab4905e3ca08e0bd03
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-08-31 10:33:10 -0300

    created ProductEditDto and ProductRegisterDto

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatolog.API/DTOs/ProductEditDto.cs
@@ -0,0 +1,27 @@
+﻿namespace DaltroStore.ProductCatolog.API.DTOs
+{
+    public class ProductEditDto
+    {
+        public Guid Id { get; init; }
+
+        public string Name { get; init; } = string.Empty;
+
+        public string Description { get; init; } = string.Empty;
+
+        public bool Active { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string Image { get; init; } = string.Empty;
+
+        public Guid CategoryId { get; init; }
+
+        public decimal Weight { get; init; }
+
+        public decimal Width { get; init; }
+
+        public decimal Height { get; init; }
+
+        public decimal Depth { get; init; }
+    }
+}

a5ba9b74e1ca1a3eb8a9f0cd689b547cf176c627
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-09-06 18:58:07 -0300

    created ProductMapper

---
+++ src/services/ProductCatolog/DaltroStore.ProductCatalog.Data/QueriesHandler/ProductMapper.cs
@@ -0,0 +1,24 @@
+﻿using DaltroStore.ProductCatalog.Application.Queries.ViewModels;
+using DaltroStore.ProductCatalog.Domain.Models;
+
+namespace DaltroStore.ProductCatalog.Infrastructure.QueriesHandler
+{
+    public static class ProductMapper
+    {
+        public static ProductViewModel ToViewModel(Product product, Category? category)
+        {
+            return new ProductViewModel(
+                    product.Id,
+                    product.Name,
+                    product.Price,
+                    product.Description,
+                    product.Image,
+                    product.Weight,
+                    product.Dimension.Width,
+                    product.Dimension.Height,
+                    product.Dimension.Depth,
+                    category?.Name ?? string.Empty
+                    );
+        }
+    }
+}
